f4867f60b2665c8f6a80db30a1bbcd8298813c24
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2023-01-09 19:41:44 +0300

    update nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,11 +3,12 @@ import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     FacebookProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
   ],
+  pages: { signIn: "/auth/signin" },
 });
